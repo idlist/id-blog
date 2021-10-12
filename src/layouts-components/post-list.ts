3f042380c50852936af4206d397a8a39a380e614
i'DLisT <i.dlist@outlook.com>
2021-10-13 01:46:34 +0800

    fix: severe bug causing pagination appears many times

--- src/layouts-components/post-list.ts
+++ src/layouts-components/post-list.ts
@@ -20,33 +20,35 @@ const PostList: Layout<PostListProps> = () => {
       const allMeta = meta.allMeta ?? []
 
       return allMeta.length
-        ? allMeta.map(postMeta => html`
+        ? html`
           ${Pagination().layout(meta, props)}
-          <div class="post">
-            <a class="post-link" href="${t.root}${routes.posts}/${postMeta.route}">
-              <h2 class="post-title">${postMeta.title}</h2>
-            </a>
-            <div class="post-meta">
-              <div class="post-time">
-                <img class="post-time-icon" src="/${routes.public}/icon/time.svg" alt="time">
-                <span>${postMeta.date.year} / ${postMeta.date.month} / ${postMeta.date.day}</span>
+          ${allMeta.map(postMeta => html`
+            <div class="post">
+              <a class="post-link" href="${t.root}${routes.posts}/${postMeta.route}">
+                <h2 class="post-title">${postMeta.title}</h2>
+              </a>
+              <div class="post-meta">
+                <div class="post-time">
+                  <img class="post-time-icon" src="/${routes.public}/icon/time.svg" alt="time">
+                  <span>${postMeta.date.year} / ${postMeta.date.month} / ${postMeta.date.day}</span>
+                </div>
+                <div class="post-tags">
+                  <img class="post-tags-icon" src="/${routes.public}/icon/tags.svg" alt="tags">
+                  ${postMeta.tags.length
+                  ? postMeta.tags.map(tag => html`
+                  <a class="post-tags-item" href="${t.root}${routes.tags}/${tag}">
+                    ${tag.replace('_', ' ')}
+                  </a>
+                  `).join('')
+                  : html`<div class="post-tags-notag">${t.use('noTags')}</div>`}
+                </div>
               </div>
-              <div class="post-tags">
-                <img class="post-tags-icon" src="/${routes.public}/icon/tags.svg" alt="tags">
-                ${postMeta.tags.length
-                ? postMeta.tags.map(tag => html`
-                <a class="post-tags-item" href="${t.root}${routes.tags}/${tag}">
-                  ${tag.replace('_', ' ')}
-                </a>
-                `).join('')
-                : html`<div class="post-tags-notag">${t.use('noTags')}</div>`}
-              </div>
+              <hr class="post-hr">
+              <div class="post-summary">${postMeta.summary}</div>
             </div>
-            <hr class="post-hr">
-            <div class="post-summary">${postMeta.summary}</div>
-          </div>
+          `).join('')}
           ${Pagination().layout(meta, props)}
-          `).join('')
+          `
         : html`<div class="post-noarticle">${t.use('noPostsLong')}</div>`
     }
   }
